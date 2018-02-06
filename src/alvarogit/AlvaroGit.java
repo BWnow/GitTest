f70719ddaf944846bc33c35bdac3d1b4006c7f80
Alvaro <elsociosm@gmail.com>
2018-02-06 17:13:15 +0100

    Primer commit

---
+++ src/alvarogit/AlvaroGit.java
@@ -0,0 +1,16 @@
+
+package alvarogit;
+
+/**
+ *
+ * @author alvaro
+ */
+public class AlvaroGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
